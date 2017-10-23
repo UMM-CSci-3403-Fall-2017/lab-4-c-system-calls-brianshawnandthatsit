b1809d7b7e7cf5a7cefe792dc1207f5c77e24c4c
GLOBBIMUS <striker1342566@gmail.com>
2017-10-22 20:16:35 -0500

    Finish summarize_tree.c
    All test pass.
    Valgrind check pass.

--- summarize_tree/summarize_tree.c
+++ summarize_tree/summarize_tree.c
@@ -5,70 +5,79 @@
 #include <dirent.h>
 #include <unistd.h>
 #include <string.h>
+#include <errno.h>
 
 
 static int num_dirs, num_regular;
 
 bool is_dir(const char* path) {
-  /*
-   * Use the stat() function (try "man 2 stat") to determine if the file
-   * referenced by path is a directory or not.  Call stat, and then use
-   * S_ISDIR to see if the file is a directory. Make sure you check the
-   * return value from stat in case there is a problem, e.g., maybe the
-   * the file doesn't actually exist.
-   */
-
-   struct stat buff;
-   stat(path, &buff);
-   int s;
-   s = S_ISDIR(buf.st_mode);
-   if(s == 0){
-     return true;
-   }else if(s == -1){
-     return false;
-   }
-
-
-
+  struct stat buf; //declare the stat struct to check if path is a directory
+  int errnum; //this int will be used to store the errno value for error messages
+  if((stat(path, &buf))==-1){ // Cheks if path is the directory and checks for errors
+    errnum = errno;
+    printf("Error checking the path: %s, the path name: %s\n", strerror(errnum), path);
+    exit(1);
+  }
+  if(S_ISDIR(buf.st_mode) != 0){ //S_ISDIR returns nonzero value if path is the directory
+    return true;
+  }else{
+    return false;
+  }
 }
 
 /*
- * I needed this because the multiple recursion means there's no way to
- * order them so that the definitions all precede the cause.
- */
+* I needed this because the multiple recursion means there's no way to
+* order them so that the definitions all precede the cause.
+*/
 void process_path(const char*);
 
 void process_directory(const char* path) {
-  /*
-   * Update the number of directories seen, use opendir() to open the
-   * directory, and then use readdir() to loop through the entries
-   * and process them. You have to be careful not to process the
-   * "." and ".." directory entries, or you'll end up spinning in
-   * (infinite) loops. Also make sure you closedir() when you're done.
-   *
-   * You'll also want to use chdir() to move into this new directory,
-   * with a matching call to chdir() to move back out of it when you're
-   * done.
-   */
+  num_dirs++; //increment the number of directories
+  struct dirent *cur_dir;
+  DIR *dir;
+  int errnum; //this int will be used to store the errno value for error messages
+  if((dir = opendir(path))==NULL){  //open the directory and checks for error
+    errnum = errno;
+    printf("Error opening directory: %s, the directory name: %s\n", strerror(errnum), path);
+    exit(1);
+  }
+  if(chdir(path)==-1){ //go to this directory and checks for error
+    errnum = errno;
+    printf("Error changing directory: %s, the directory name: %s\n", strerror(errnum), path);
+    exit(1);
+  }
+  while((cur_dir = readdir(dir))!=NULL){ //loop through the entries in the directory
 
-   //Declare struct dirent
-  // struct dirent *dir_data;
-  // DIR *dir
+    // skip if the entries are ".." or "."
+    if((strcmp((cur_dir->d_name),"..")==0)||(strcmp((cur_dir->d_name),".")==0)){
+      continue;
+    }
 
+    //recursive call to check the entry
+    process_path(cur_dir->d_name);
 
+  }
 
+  if(chdir("..")==-1){ //go to the parent directory and checks for error
+    errnum = errno;
+    printf("Error changing directory: %s, the directory name: %s\n", strerror(errnum), "..");
+    exit(1);
+  }
 
+  if(closedir(dir)==-1){ //closes directory and checks for error
+    errnum = errno;
+    printf("Error closing directory: %s, the directory name: %s\n", strerror(errnum), path);
+    exit(1);
+  }
 }
 
 void process_file(const char* path) {
-  /*
-   * Update the number of regular files.
-   */
+  num_regular++; //update the number of regular files
 }
 
 void process_path(const char* path) {
   if (is_dir(path)) {
-    process_directory(path);
+    if(path!="."&&path!="..")process_directory(path);
   } else {
     process_file(path);
   }
